c47e6393e81d8da44c192d85192eadb08039477b
Mateus Rodrigues do Nascimento <90523108+mateusrdn@users.noreply.github.com>
2022-08-05 11:39:30 -0300

    Commit final

--- src/main/java/com/dio/bootcamp/academia/digital/entity/Aluno.java
+++ src/main/java/com/dio/bootcamp/academia/digital/entity/Aluno.java
@@ -12,6 +12,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CPF;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -30,13 +36,21 @@ public class Aluno {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank(message = "Preencha o campo corretamente")
+	@Size(min = 3, max = 50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres")
 	private String nome;
 	
 	@Column(unique = true)
+	@NotBlank(message = "Preencha o campo corretamente")
+	@CPF(message = "'${validatedValue}' é invalido")
 	private String cpf;
 	
+	@NotBlank(message = "Preencha o campo corretamente")
+	@Size(min = 3, max = 50, message = "'${validatedValue}' precisa entar entre {min} e {max} caracteres.")
 	private String bairro;
 	
+	@NotNull(message = "Preencha o campo corretamente")
+	@Past(message = "Data '${validatedValue}' é invalida")
 	private LocalDate dataDeNascimento;
 	
 	@OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
